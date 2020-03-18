93818c936ee7e2f1ba1b315578bde363a7d43d05
Xiao Hanyu <xiaohanyu1988@gmail.com>
2020-03-18 16:24:16 +0800

    Fix conflicting license.
    Thanks Jeremy Korwin-Zmijowski (https://guix.gnu.org/) for reporting.

--- ac-geiser.el
+++ ac-geiser.el
@@ -1,24 +1,6 @@
 ;;; ac-geiser.el --- Emacs auto-complete backend for geiser.
 
-;; Copyright (C) 2013  Xiao Hanyu <xiaohanyu1988@gmail.com>
-
-;; Author: Xiao Hanyu <xiaohanyu1988@gmail.com>
-;; URL: https://github.com/xiaohanyu/ac-geiser
-;; Version: 0.1
-;; Package-Requires: ((geiser "0.5") (auto-complete "1.4"))
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;; Copyright (C) 2013-2020  Xiao Hanyu <xiaohanyu1988@gmail.com>
 
 ;;; Commentary:
 
